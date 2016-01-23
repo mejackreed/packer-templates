79ee9ae30c4a56e5bb4d152fd499e870b398ffe7
Jack Reed <pjreed@stanford.edu>
2016-01-23 08:22:05 -0800

    add a geoblacklight installation

---
+++ ubuntu-14.04.3/scripts/geoblacklight.sh
@@ -0,0 +1,13 @@
+mkdir pregenerated
+
+cd pregenerated
+
+rails new demo_geoblacklight_app -m https://raw.githubusercontent.com/geoblacklight/geoblacklight/master/template.rb
+
+cd demo_geoblacklight_app
+
+bundle exec rake jetty:download
+
+bundle exec rake jetty:unzip
+
+bundle exec rake geoblacklight:configure_jetty

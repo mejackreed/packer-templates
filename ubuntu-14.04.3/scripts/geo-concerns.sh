9df7e97b3fc9a19e87bc80adc76e0e41175226e3
Jack Reed <phillipjreed@gmail.com>
2016-06-08 16:42:23 -0400

    create custom rakefile

--- ubuntu-14.04.3/scripts/geo-concerns.sh
+++ ubuntu-14.04.3/scripts/geo-concerns.sh
@@ -5,7 +5,7 @@ echo "Creating a stock Geo Concerns app"
 # create a curation-concerns app
 rails new geo-concerns-demo --skip-spring
 cd geo-concerns-demo
-sudo gem install simple_mapnik
+gem install simple_mapnik
 echo "gem 'curation_concerns', '1.0.0.beta7'" >> Gemfile
 echo "gem 'geo_concerns', '0.0.6'" >> Gemfile
 bundle install
@@ -14,15 +14,42 @@ yes Y | rails generate geo_concerns:install
 rake db:migrate
 
 # start redis
-sudo /etc/init.d/redis-server start
+/etc/init.d/redis-server start
 
-SHARED_DIR="/vagrant"
+rm Rakefile
+cat > Rakefile <<- EOM
+# Add your own tasks in files placed in lib/tasks ending in .rake,
+# for example lib/tasks/capistrano.rake, and they will automatically be available to Rake.
 
-# Copy in new Rakefile
-yes | cp -rf $SHARED_DIR/install_scripts/Rakefile .
+require File.expand_path('../config/application', __FILE__)
+
+Rails.application.load_tasks
+
+require 'solr_wrapper/rake_task'
+require 'fcrepo_wrapper/rake_task'
+
+namespace :demo do
+  desc "Run fedora, solr, and rails servers for Geo Concerns"
+  task :servers do
+    with_server 'development' do
+      begin
+        `rails s -b 0.0.0.0`
+      rescue Interrupt
+        puts "Shutting down..."
+      end
+    end
+  end
+  desc "Setup fedora and solr for Geo Concerns"
+  task :setup do
+    with_server 'development' do
+    end
+  end
+end
+
+EOM
 
 # Make solr core nonpersistent. Causes errors when solr-wrapper is restarted.
 sed -i 's/persist: true/persist: false/g' .solr_wrapper
 
 # setup fedora and solr
-rake demo:setup
+rake demo:server

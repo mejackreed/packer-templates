d757a657a221c17c54ece35f958c33c9c9aff440
Jack Reed <phillipjreed@gmail.com>
2017-08-11 22:08:17 -0400

    update to 16.04.3

--- ubuntu-14.04.5/scripts/java.sh
+++
@@ -1,8 +0,0 @@
-apt-add-repository ppa:webupd8team/java -y
-apt-get update
-echo debconf shared/accepted-oracle-license-v1-1 select true | \
-  sudo debconf-set-selections
-echo debconf shared/accepted-oracle-license-v1-1 seen true | \
-  sudo debconf-set-selections
-
-apt-get -y install oracle-java8-installer
---
+++ ubuntu-16.04.3/scripts/java.sh
@@ -0,0 +1,5 @@
+# echo 'Installing Java8'
+sudo add-apt-repository ppa:openjdk-r/ppa -y
+sudo apt-get update
+sudo apt-get -y install openjdk-8-jdk
+java -version
